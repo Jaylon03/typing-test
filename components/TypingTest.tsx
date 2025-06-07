2719642a15bb82f215eb1ed1831b4162a564c66b
Jaylon Nelson <jaylonnell03@gmail.com>
2025-06-07 00:14:38 -0500

    added automatic quote rotation

--- components/TypingTest.tsx
+++ components/TypingTest.tsx
@@ -1,9 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import QuoteDisplay from "./QuoteDisplay";
 
+    const quotes = [
+        "First quote to type.",
+        "Another one to practice with.",
+        "Typing tests are fun and useful.",
+        "Almost done, one more to go!"
+    ];
+
+
 const TypingTest = () => {
-    const [quote] = useState("Type this sentence exactly. ")
+    const [quoteIndex, setQuoteIndex] = useState(0);
     const [userInput, setUserInput] = useState("");
+    const quote = quotes[quoteIndex];
+
+    useEffect(() => {
+        if (userInput === quote) {
+            setTimeout(() => {
+                const nextIndex = (quoteIndex + 1) % quotes.length;
+                setQuoteIndex(nextIndex);
+                setUserInput("");
+            }, 200);
+        }
+
+    }, [userInput, quote, quoteIndex]);
 
 
     return (
@@ -15,7 +35,7 @@ const TypingTest = () => {
                 value={userInput}
                 onChange={e => setUserInput(e.target.value)}
                 autoFocus
-                className="border p-2 rounded text-lg w-full font-mono"
+                className="border p-2 rounded text-lg w-140 font-mono"
                 spellCheck={false}
 
             />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,8 @@ import TypingTest from "../../components/TypingTest";
 
 export default function Home() {
   return (
-    <div className="min-h-screen p-8 font-[family-name:var(--font-geist-sans)]">
-      
-      
+    <div className="min-h-screen p-8 font-[family-name:var(--font-geist-sans)]"> 
+      <ThemeToggle/>
 
       <main className="flex flex-col items-center justify-center min-h-screen">
       
